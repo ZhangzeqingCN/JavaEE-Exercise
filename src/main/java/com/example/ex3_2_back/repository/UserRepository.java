3a0ac5fde09bc74814d0caceee666d9490f2619f
张泽清 <1804924578@qq.com>
2023-04-01 17:29:01 +0800

    登陆注册页面

--- src/main/java/com/example/ex3_2_back/repository/UserRepository.java
+++ src/main/java/com/example/ex3_2_back/repository/UserRepository.java
@@ -6,5 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByIdAndName(int id, String name);
+    Optional<User> findByIdAndName(Integer id, String name);
+
+    Optional<User> findByNameAndPassword(String name, String password);
+
+    Optional<User> findByIdAndPassword(Integer id, String password);
+
+    boolean existsByNameAndPassword(String name, String password);
 }
