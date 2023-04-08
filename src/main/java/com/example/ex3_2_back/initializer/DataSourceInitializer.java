731b4e27d774445e52152bff9577ca133a6c8b79
HYT111222 <HeYiTing2023@outlook.com>
2023-04-08 17:55:12 +0800

    修改了shipment表并插入初始数据，完成部分物流追踪功能

--- src/main/java/com/example/ex3_2_back/initializer/DataSourceInitializer.java
+++ src/main/java/com/example/ex3_2_back/initializer/DataSourceInitializer.java
@@ -2,18 +2,25 @@ package com.example.ex3_2_back.initializer;
 
 import com.example.ex3_2_back.entity.Movie;
 import com.example.ex3_2_back.entity.User;
+import com.example.ex3_2_back.entity.Shipment;
 import com.example.ex3_2_back.repository.MovieRepository;
 import com.example.ex3_2_back.repository.UserRepository;
+import com.example.ex3_2_back.repository.ShipmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * 数据库初始化
+ */
 @Component
 public class DataSourceInitializer implements CommandLineRunner {
 
     UserRepository userRepository;
 
     MovieRepository movieRepository;
+
+    ShipmentRepository shipmentRepository;
 
     @Autowired
     public void setMovieRepository(MovieRepository movieRepository) {
@@ -23,6 +30,37 @@ public class DataSourceInitializer implements CommandLineRunner {
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setShipmentRepository(ShipmentRepository shipmentRepository) {
+        this.shipmentRepository = shipmentRepository;
+    }
+
+    /**
+     * Integer id;
+     *     User fromUser;
+     *     User toUser;
+     *     String fromAddress;
+     *     String toAddress;
+     *     String parcelTrace;
+     */
+    void initializeShipments() {
+        for (int i = 0; i < 50; i++) {
+            var ship = Shipment.builder()
+                    .id(i)
+                    .fromUser("lisa")
+                    .toUser("lisa")
+                    .fromAddress(String.valueOf(i*4))
+                    .toAddress(String.valueOf(i*4))
+                    .toPhone("12344")
+                    .fromPhone("189740")
+                    .parcelTrace(String.valueOf(i*5))
+                    .build();
+            shipmentRepository.save(ship);
+        }
+
+//        shipmentRepository.save(Shipment.builder().name("shipment").build());
     }
 
     void initializeUsers() {
@@ -54,5 +92,6 @@ public class DataSourceInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
         initializeUsers();
         initializeMovies();
+        initializeShipments();
     }
 }
