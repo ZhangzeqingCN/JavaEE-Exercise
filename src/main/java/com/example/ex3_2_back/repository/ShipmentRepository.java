087a9ecf264831e24724dcd47404be971f0a96b2
HYT111222 <HeYiTing2023@outlook.com>
2023-04-10 19:02:44 +0800

    修改了shipment表并插入初始数据，完成部分物流追踪功能

--- src/main/java/com/example/ex3_2_back/repository/ShipmentRepository.java
+++ src/main/java/com/example/ex3_2_back/repository/ShipmentRepository.java
@@ -1,8 +1,22 @@
 package com.example.ex3_2_back.repository;
 
+import com.example.ex3_2_back.entity.Movie;
 import com.example.ex3_2_back.entity.Shipment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ShipmentRepository extends JpaRepository<Shipment, Integer> {
+    Optional<Shipment> findById(String id);
 
+
+    List<Shipment> findByFromUser(String fromUser);
+
+    List<Shipment> findByToUser(String toUser);
+
+//    @Query(value = "from Shipment m order by m.id, m.fromUser, m.toUser")
+//    List<Shipment> myFind();
 }
