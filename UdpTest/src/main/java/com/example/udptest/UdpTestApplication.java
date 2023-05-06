b69253ababfee38e63c21cd07a0321c75b593ff4
张泽清 <1804924578@qq.com>
2023-05-06 13:51:25 +0800

    udp test

---
+++ UdpTest/src/main/java/com/example/udptest/UdpTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.udptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdpTestApplication.class, args);
+    }
+
+}
