4d2c12004b7d2e17acc6dbfbbc4bd955723720ed
张泽清 <1804924578@qq.com>
2023-05-06 13:51:25 +0800

    udp test

---
+++ UdpTest/src/test/java/com/example/udptest/UdpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdpTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
