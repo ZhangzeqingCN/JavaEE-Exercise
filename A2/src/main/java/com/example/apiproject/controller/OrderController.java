ba2b3ebc75e60b39b3421e2c929857f797ff9e00
HYT111222 <HeYiTing2023@outlook.com>
2023-06-05 21:01:14 +0800

    添加部分实体等

---
+++ A2/src/main/java/com/example/apiproject/controller/OrderController.java
@@ -0,0 +1,17 @@
+package com.example.apiproject.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther HYT
+ * @Date 2023/6/5
+ * @Desc
+ */
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class OrderController {
+
+}
---
+++ A2/src/main/java/com/example/apiproject/service/OrderService.java
@@ -0,0 +1,14 @@
+package com.example.apiproject.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Auther HYT
+ * @Date 2023/6/5
+ * @Desc
+ */
+@Service
+@Slf4j
+public class OrderService {
+}
